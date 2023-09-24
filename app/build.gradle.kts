4938d44070048a5d873facf385e8752091fe80e9
aman <aman98sahay@>
2023-09-24 13:12:13 +0530

    removing unused ndk googletest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,5 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    testImplementation("com.android.ndk.thirdparty:googletest:1.11.0-beta-1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
